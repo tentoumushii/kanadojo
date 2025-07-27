e8437be01221c89aec4db12891e1df6b1a0f0d37
Aldiar Dauletuly <aldidauletuly@gmail.com>
2025-07-27 18:56:34 +0500

    fixing vercel deployment

--- app/vocabulary/[set]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Loader from '@/components/reusable/Skeletons/Loader';
-
-export default function VocabSetDictLoaderPage() {
-  return <Loader />;
-}
--- app/vocabulary/[set]/page.tsx
+++
@@ -1,5 +0,0 @@
-import SetDictionary from '@/components/Dojo/Vocab/SetDictionary';
-
-export default function VocabDictPage() {
-  return <SetDictionary />;
-}
